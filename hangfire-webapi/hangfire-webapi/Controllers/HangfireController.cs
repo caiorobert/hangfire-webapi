a85f6930ba2dec65d1271d7357e8767032ffcef4
Caio Robert <caio.robert@live.com>
2021-05-13 00:56:12 -0300

    First commit: start with hangfire, using fire and forget jobs

---
+++ hangfire-webapi/hangfire-webapi/Controllers/HangfireController.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hangfire_webapi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HangfireController : ControllerBase
+    {
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Welcome()
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendWelcomeEmail("Welcome to our app"));
+
+            return Ok($"Job ID: {jobId}. Welcome email sent to the user");
+        }
+
+        public void SendWelcomeEmail(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
